42aea1fb131154db99445b917e6fbd6d5fc18f94
beckdaniel <daniel.e.beck@gmail.com>
2013-09-12 16:36:06 +0100

    sent tokenize

--- cleaning/sent_clean.sh
+++ cleaning/sent_clean.sh
@@ -6,6 +6,13 @@ gunzip $IN |
 # Filter lines < 5
 awk '{if (NF > 5) print $0;}' |
 
+# Replace carriage return
+sed 's|\r||g' |
+
+# Sent tokenize
+python -c "import nltk, sys; print '\n'.join([sent.replace('\n','') for sent in nltk.sent_tokenize(sys.stdin.read())]);" |
+
+
 # Filter rubbish
 sed '/[^[:alnum:][:space:]\.,:;\?!)(\$]/d' |
 sed -n 's/[[:alpha:]]/&/p' |
